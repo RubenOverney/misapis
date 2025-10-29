a166e1c0e28cc4b6105758a856e51bec322850ff
RubenOverney <125749722+RubenOverney@users.noreply.github.com>
2025-10-28 23:31:40 -0500

    Delete Programacion_6To_B directory

--- Programacion_6To_B/rutas/index.routes.js
+++
@@ -1,9 +0,0 @@
-import ejemplo from './ejemplo.routes.js';
-import { Router } from 'express';
-const indexRouters = Router();
-
-indexRouters.use('/ejemplo', ejemplo);
-
-
-
-export default indexRouters;
